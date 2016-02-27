9602b60495d73df77347a20533a4341a5194fac8
nick11roberts <n3robert@ucsd.edu>
2016-02-27 15:11:35 -0800

    Gradle scripts working nice and proper.

---
+++ src/main/java/Cli.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Cli {
+   private static Scanner scanner = new Scanner(System.in);
+
+   public static void main(String[] args) {
+
+      int p = 0;
+
+      System.out.println("dfgjhdfg");
+        Scanner scan = new Scanner(System.in);
+        p = scan.nextInt();
+
+        System.out.println(": " + p);
+   }
+}
